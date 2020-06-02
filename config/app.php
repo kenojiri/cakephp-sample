bc339cb843faa40439106eb3fa9d8fb943f8d203
Ken Ojiri <ozzozz@gmail.com>
2020-06-02 03:53:03 +0000

    output app logs to stdout/stderr

--- config/app.php
+++ config/app.php
@@ -4,7 +4,8 @@ use Cake\Cache\Engine\FileEngine;
 use Cake\Database\Connection;
 use Cake\Database\Driver\Mysql;
 use Cake\Error\ExceptionRenderer;
-use Cake\Log\Engine\FileLog;
+use Cake\Console\ConsoleOutput;
+use Cake\Log\Engine\ConsoleLog;
 use Cake\Mailer\Transport\MailTransport;
 
 return [
@@ -338,26 +339,26 @@ return [
      */
     'Log' => [
         'debug' => [
-            'className' => FileLog::class,
-            'path' => LOGS,
-            'file' => 'debug',
+            'className' => ConsoleLog::class,
+            'stream' => 'php://stdout',
+            'outputAs' => ConsoleOutput::PLAIN,
             'url' => env('LOG_DEBUG_URL', null),
             'scopes' => false,
             'levels' => ['notice', 'info', 'debug'],
         ],
         'error' => [
-            'className' => FileLog::class,
-            'path' => LOGS,
-            'file' => 'error',
+            'className' => ConsoleLog::class,
+            'stream' => 'php://stderr',
+            'outputAs' => ConsoleOutput::PLAIN,
             'url' => env('LOG_ERROR_URL', null),
             'scopes' => false,
             'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
         ],
         // To enable this dedicated query log, you need set your datasource's log flag to true
         'queries' => [
-            'className' => FileLog::class,
-            'path' => LOGS,
-            'file' => 'queries',
+            'className' => ConsoleLog::class,
+            'stream' => 'php://stdout',
+            'outputAs' => ConsoleOutput::PLAIN,
             'url' => env('LOG_QUERIES_URL', null),
             'scopes' => ['queriesLog'],
         ],
